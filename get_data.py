ef47a66cd7dc51b2de1f14e97fe7fa56e48b52c8
Piotr Wittchen <piotr@wittchen.io>
2020-03-15 11:23:12 +0100

    reorganizing code

--- get_data.py
+++ get_data.py
@@ -5,5 +5,7 @@ from bs4 import BeautifulSoup
 url = 'https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2'
 request = requests.get(url)
 html = BeautifulSoup(request.text, 'html.parser')
-register = json.loads(html.find(id="registerData").string)
-print(register['data'])
+text = html.find(id="registerData").string
+json = json.loads(text)
+data = json['data']
+print(data)
