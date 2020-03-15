d495e02339235be4006cc06a519ded682107ed37
Piotr Wittchen <piotr@wittchen.io>
2020-03-15 12:12:32 +0100

    adding summary and printing regions

--- get_data.py
+++ get_data.py
@@ -1,11 +1,27 @@
 import requests
 import json
+import sys
 from bs4 import BeautifulSoup
+
+if len(sys.argv) != 2:
+  sys.exit()
 
 url = 'https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2'
 request = requests.get(url)
 html = BeautifulSoup(request.text, 'html.parser')
 text = html.find(id="registerData").string
-json = json.loads(text)
-data = json['data']
-print(data)
+register_data = json.loads(text)
+data = register_data['data']
+parsed_data = register_data['parsedData']
+
+if str(sys.argv[1]) == 'summary':
+  for item in json.loads(parsed_data):
+    if item['Województwo'] == 'Cała Polska':
+      infected = int(item['Liczba'])
+      dead =  0 if not item['Liczba zgonów'] else int(item['Liczba zgonów'])
+      break
+
+  print('infected: {}, dead: {} '.format(infected, dead))
+
+if str(sys.argv[1]) == 'regions':
+  print(data)
